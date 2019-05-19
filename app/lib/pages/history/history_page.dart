069a4aaf68f142a83b1cd89b0b141d98dbf5555a
Paddo <me@paddo.org>
2019-05-20 03:54:32 +1000

    history page

--- app/lib/pages/history/history_page.dart
+++ app/lib/pages/history/history_page.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:functional_widget_annotation/functional_widget_annotation.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:functional_widget_annotation/functional_widget_annotation.dart';
+import 'package:time_tracking/core/backend_service.dart';
+import 'package:time_tracking/core/service_locator.dart';
+import 'package:intl/intl.dart';
 
 part 'history_page.g.dart';
 
 @widget
 Widget historyPage() {
-  return Container();
+  final historyItems = useStream(sl<BackendService>().getHistory()).data ?? [];
+
+  return Container(
+    child: ListView(
+      children: historyItems.map((item) => HistoryRow(item)).toList(),
+    ),
+  );
 }
+
+@widget
+Widget historyRow(HistoryItem historyItem) => Row(
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Text(DateFormat.yMd().add_jm().format(historyItem.dateTime)),
+        Text(historyItem.duration.toString()),
+      ],
+    );
